76d799d64d16955a73bdf1e74648aac580a8a8f2
marciondg <53331055+marciondg@users.noreply.github.com>
2019-12-30 02:42:40 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,34 @@
+while True:
+    print("""Opciones
+    Ingrese '+' para sumar dos numeros
+    Ingrese '-' para restar dos numeros
+    Ingrese '*' para multiplicar dos numeros
+    Ingrese '/' para dividir dos numeros
+    Ingrese 'salir' para finalizar""")
+
+    teclado=input(":")
+
+    if teclado=="salir" or teclado=="Salir":
+        break
+    elif teclado=="+":
+        num1=float(input("Ingrese un numero:"))
+        num2=float(input("Ingrese otro numero:"))
+        result=str(num1+num2)
+        print("Respuesta: "+result)
+    elif teclado=="-":
+        num1=float(input("Ingrese un numero:"))
+        num2=float(input("Ingrese otro numero:"))
+        result=str(num1-num2)
+        print("Respuesta: "+result)
+    elif teclado=="*":
+        num1=float(input("Ingrese un numero:"))
+        num2=float(input("Ingrese otro numero:"))
+        result=str(num1*num2)
+        print("Respuesta: "+result)
+    elif teclado=="/":
+        num1=float(input("Ingrese un numero:"))
+        num2=float(input("Ingrese otro numero:"))
+        result=str(num1/num2)
+        print("Respuesta: "+result)
+    else:
+        print("Opcion inválida")
